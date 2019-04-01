0c090f83b9ce18ed26468ef8a123671ef8892b4c
Justin Tran <jctran@princeton.edu>
2019-04-01 01:21:14 -0400

    Added ego-graph constructor for largest in-degree artist for each dataset

--- egoGraph.py
+++ egoGraph.py
@@ -3,13 +3,14 @@ import networkx
 import matplotlib.pyplot as plt
 import matplotlib
 import collections
+from operator import itemgetter
 
 # Set font sizes of plots
 font = {'family': 'normal', 'size': 6}
 matplotlib.rc('font', **font)
 
 # Parse through spreadsheet data
-with open('whosampled30k.csv', mode='r', encoding="cp850") as dataSamples:
+with open('whosampled1250.csv', mode='r', encoding="cp850") as dataSamples:
 	dataSamples = csv.reader(dataSamples)
 	# Read out header with row
 	header = next(dataSamples)
@@ -77,12 +78,12 @@ for node in diG.nodes:
 	most_sampled[node] = diG.in_degree(node)
 	most_samples[node] = diG.out_degree(node)
 
-# # Print number of times sampled
-# # print(sorted(most_sampled.items(), key=lambda sample: sample[1]))
+# Print number of times sampled
+# print(sorted(most_sampled.items(), key=lambda sample: sample[1]))
 # # Print number of times sampling something
-# # print(sorted(most_samples.items(), key=lambda sample: sample[1]))
+# print(sorted(most_samples.items(), key=lambda sample: sample[1]))
 # # Print associated genre
-# # print(networkx.get_node_attributes(diG, 'genre'))
+# print(networkx.get_node_attributes(diG, 'genre'))
 
 # # Get all intragenre and intergenre samples
 # intraGenre = []
@@ -107,12 +108,44 @@ for node in diG.nodes:
 # # print(betweenness)
 
 # Display graph
-layout = networkx.shell_layout(diG)
+# largest_hub is the artist with the most times sampled
+(largest_hub, degree) = sorted(most_sampled.items(), key=itemgetter(1))[-2]
+# largest_hub = 'The Winstons'
+# Largest hub's samplers
+largest_hub_samplers = [hub_sampler[0] for hub_sampler in networkx.edges(diG)
+	if largest_hub in hub_sampler]
+hub_ego = networkx.ego_graph(diG, largest_hub)
+# Construct ego graph links
+for link in links:
+	if largest_hub in link:
+		hub_ego.add_node(link[0], genre=link[3])
+		hub_ego.add_edge(u_of_edge=largest_hub, v_of_edge=link[0], audioElem=link[2])
+# Construct links of edges 1 away from ego hub
+original_hub_nodes = dict(networkx.nodes(hub_ego))
+for artist in original_hub_nodes:
+	if artist == largest_hub:
+		continue
+	for link in links:
+		if artist in link and link[0] not in original_hub_nodes:
+			hub_ego.add_node(link[0], genre=link[3])
+			hub_ego.add_edge(u_of_edge=link[0], v_of_edge=artist, audioElem=link[2])
+		if artist in link and link[1] not in original_hub_nodes:
+			hub_ego.add_node(link[1], genre=link[4])
+			hub_ego.add_edge(u_of_edge=artist, v_of_edge=link[1], audioElem=link[2])
+# All samplers in ego hub network
+largest_hub_samplers = [hub_sampler[0] for hub_sampler in networkx.edges(hub_ego)]
+pos = networkx.spring_layout(hub_ego)
+
 # Create edge labels
-edge_labels = networkx.get_edge_attributes(diG, 'audioElem')
-networkx.draw_networkx_edge_labels(diG, layout, edge_labels, font_size=8)
+edge_labels = networkx.get_edge_attributes(hub_ego, 'audioElem')
+networkx.draw_networkx_edge_labels(hub_ego, pos, edge_labels, font_size=8)
 # Create color map by genre by mapping respective genres to ints
-color_map_genre = [hash(genre) for genre in networkx.get_node_attributes(diG, 'genre').values()]
-networkx.draw(diG, layout, with_labels=True, cmap=plt.cm.RdYlBu, node_color=color_map_genre, 
-	node_size=[v * 100 for v in most_sampled.values()], font_size=8)
-plt.show(diG)
+color_map_genre = [hash(genre) for genre in networkx.get_node_attributes(hub_ego, 'genre').values()]
+
+# Draw graph
+networkx.draw(hub_ego, pos, cmap=plt.cm.RdYlBu, node_color=color_map_genre, node_size=100, with_labels=True, font_size=8)
+del color_map_genre[0]
+# Draw ego graph based on genre color
+networkx.draw_networkx_nodes(hub_ego, pos, nodelist=largest_hub_samplers, node_size=100,
+	cmap=plt.cm.RdYlBu, node_color=color_map_genre)
+plt.show()
