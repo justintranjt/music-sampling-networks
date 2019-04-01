33c6d985b6dc8167ff2982e0ee46850de5f0dfba
Justin Tran <jctran@princeton.edu>
2019-04-01 13:26:57 -0400

    Compiled ego graphs of top 5 sampled artists and top sampling artists

--- egoGraph.py
+++ egoGraph.py
@@ -10,7 +10,7 @@ font = {'family': 'normal', 'size': 6}
 matplotlib.rc('font', **font)
 
 # Parse through spreadsheet data
-with open('whosampled1250.csv', mode='r', encoding="cp850") as dataSamples:
+with open('whosampled5k.csv', mode='r', encoding="cp850") as dataSamples:
 	dataSamples = csv.reader(dataSamples)
 	# Read out header with row
 	header = next(dataSamples)
@@ -84,6 +84,8 @@ for node in diG.nodes:
 # print(sorted(most_samples.items(), key=lambda sample: sample[1]))
 # # Print associated genre
 # print(networkx.get_node_attributes(diG, 'genre'))
+	# Most sampled artists: ('Beside', 203), ('Lyn Collins', 233), ('Public Enemy', 258), ('The Winstons', 272), ('James Brown', 599)
+	# Most sampling artists: ('J Dilla', 65), ('Lil B', 65), ('DJ Shadow', 77), ('Madlib', 88), ('Girl Talk', 99)
 
 # # Get all intragenre and intergenre samples
 # intraGenre = []
@@ -109,17 +111,17 @@ for node in diG.nodes:
 
 # Display graph
 # largest_hub is the artist with the most times sampled
-(largest_hub, degree) = sorted(most_sampled.items(), key=itemgetter(1))[-2]
-# largest_hub = 'The Winstons'
+# (largest_hub, degree) = sorted(most_samples.items(), key=itemgetter(1))[-2]
+largest_hub = 'Girl Talk'
 # Largest hub's samplers
 largest_hub_samplers = [hub_sampler[0] for hub_sampler in networkx.edges(diG)
 	if largest_hub in hub_sampler]
-hub_ego = networkx.ego_graph(diG, largest_hub)
+hub_ego = networkx.ego_graph(diG, largest_hub, distance=20)
 # Construct ego graph links
 for link in links:
 	if largest_hub in link:
 		hub_ego.add_node(link[0], genre=link[3])
-		hub_ego.add_edge(u_of_edge=largest_hub, v_of_edge=link[0], audioElem=link[2])
+		hub_ego.add_edge(u_of_edge=link[0], v_of_edge=largest_hub, audioElem=link[2])
 # Construct links of edges 1 away from ego hub
 original_hub_nodes = dict(networkx.nodes(hub_ego))
 for artist in original_hub_nodes:
@@ -128,24 +130,29 @@ for artist in original_hub_nodes:
 	for link in links:
 		if artist in link and link[0] not in original_hub_nodes:
 			hub_ego.add_node(link[0], genre=link[3])
-			hub_ego.add_edge(u_of_edge=link[0], v_of_edge=artist, audioElem=link[2])
+			hub_ego.add_edge(u_of_edge=link[0], v_of_edge=artist)
 		if artist in link and link[1] not in original_hub_nodes:
 			hub_ego.add_node(link[1], genre=link[4])
-			hub_ego.add_edge(u_of_edge=artist, v_of_edge=link[1], audioElem=link[2])
+			hub_ego.add_edge(u_of_edge=artist, v_of_edge=link[1])
+
+
 # All samplers in ego hub network
 largest_hub_samplers = [hub_sampler[0] for hub_sampler in networkx.edges(hub_ego)]
 pos = networkx.spring_layout(hub_ego)
 
 # Create edge labels
 edge_labels = networkx.get_edge_attributes(hub_ego, 'audioElem')
-networkx.draw_networkx_edge_labels(hub_ego, pos, edge_labels, font_size=8)
+networkx.draw_networkx_edge_labels(hub_ego, pos, edge_labels, font_size=4)
 # Create color map by genre by mapping respective genres to ints
 color_map_genre = [hash(genre) for genre in networkx.get_node_attributes(hub_ego, 'genre').values()]
 
 # Draw graph
-networkx.draw(hub_ego, pos, cmap=plt.cm.RdYlBu, node_color=color_map_genre, node_size=100, with_labels=True, font_size=8)
+networkx.draw(hub_ego, pos, cmap=plt.cm.RdYlBu, node_color=color_map_genre, node_size=50, with_labels=True, font_size=10)
 del color_map_genre[0]
+
 # Draw ego graph based on genre color
-networkx.draw_networkx_nodes(hub_ego, pos, nodelist=largest_hub_samplers, node_size=100,
-	cmap=plt.cm.RdYlBu, node_color=color_map_genre)
+networkx.draw_networkx_nodes(hub_ego, pos, nodelist=largest_hub_samplers, node_size=50,
+	cmap=plt.cm.RdYlBu)
+plt.title('Ego Graph for 1st Most-Sampling Artist in Network (Girl Talk)', size=12)
+# plt.savefig('egoGraphMostSampling1GirlTalk.png', dpi=199)
 plt.show()
